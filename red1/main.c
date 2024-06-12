82d307414ce10e9a10c411ade031ca41dbd28a3e
tobysimpson <tobygeneva@hotmail.com>
2024-06-12 14:04:52 +0200

    Initial Commit

---
+++ red1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  red1
+//
+//  Created by toby on 12.06.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
